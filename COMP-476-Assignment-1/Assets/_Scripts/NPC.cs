42c5c6a82235776850d94dbf4c0976e8b2c98f3a
Adrien Kamran <adrien.kamran@gmail.com>
2022-02-13 17:22:22 -0500

    [R2] Basic kinematic seek implemented
    More advanced heuristics still need to be done...

--- COMP-476-Assignment-1/Assets/_Scripts/NPC.cs
+++ COMP-476-Assignment-1/Assets/_Scripts/NPC.cs
@@ -4,46 +4,76 @@ using UnityEngine;
 
 public class NPC : MonoBehaviour
 {
-    public float moveSpeed = 15f;
+    public float moveSpeed = 1f;
+    public float maxSpeed = 15f;
     public float rotationSpeed = 300f;
     public Rigidbody rb;
+    public GameObject targetObj;
+    public float frequency = 3f;
+    public float stopRadius = 3f;
+    public float slowRadius = 6f;
+
+    private float timeRemaining;
+    private Transform target;
 
     Vector3 movement;
-    public float frequency = 3f;
-    private float timeRemaining;
 
     // Start is called before the first frame update
     void Start()
     {
         movement = Vector3.zero;
         rb = GetComponent<Rigidbody>();
+        target = targetObj.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Wandering behaviour
-        timeRemaining -= Time.deltaTime;
-        if (timeRemaining <= 0)
-        {
-            movement = new Vector3(Random.Range(-1f, 1f), 0, Random.Range(-1f, 1f));
-            //movement.Normalize();
-            timeRemaining += frequency;
-        }
+
     }
 
     private void FixedUpdate()
     {
-        // Move the NPC in the chosen direction at a set move speed
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-        // If the NPC is moving...
+        movement = KinematicSeek();
+        Move();
+        LookAtMovement();
+    }
+
+    // Look in the direction of movement
+    void LookAtMovement()
+    {
+        // If the player is moving...
         if (movement != Vector3.zero)
         {
-            // ... Create a quaternion which sets where the NPC should be facing
+            // ... Create a quaternion which sets where the player should be facing
             Quaternion targetRotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(movement), rotationSpeed * Time.fixedDeltaTime);
             // Rotate towards that direction at a set rotation speed
             rb.MoveRotation(targetRotation);
         }
     }
+
+    void Move()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    Vector3 KinematicSeek()
+    {
+        Vector3 desiredVelocity = target.position - transform.position;
+        float distance = desiredVelocity.magnitude;
+        desiredVelocity = desiredVelocity.normalized * maxSpeed;
+
+        if (distance <= stopRadius)
+        {
+            desiredVelocity *= 0;
+        }
+        else if (distance < slowRadius)
+        {
+            desiredVelocity *= (distance / slowRadius);
+        }
+
+        return desiredVelocity;
+    }
+
 
 }
